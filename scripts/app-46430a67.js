07edf13b43bed33082e5e75f84c6c47030072e76
Andre Caetano <webandrecaetano@gmail.com>
2015-04-19 11:19:07 -0300

    complete examples

--- scripts/app-46430a67.js
+++
@@ -1,3 +0,0 @@
-/******/!function(t){function o(e){if(r[e])return r[e].exports;var n=r[e]={exports:{},id:e,loaded:!1};return t[e].call(n.exports,n,n.exports,o),n.loaded=!0,n.exports}// webpackBootstrap
-/******/
-var r={};return o.m=t,o.c=r,o.p="",o(0)}([function(t,o,r){"use strict";r(1)},function(){"use strict";$(document).ready(function(){console.log($)})}]);
