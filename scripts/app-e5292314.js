5646854f6ac0f33eb58e5f54e8641af2309cf5a7
Andre Caetano <webandrecaetano@gmail.com>
2015-04-18 22:15:06 -0300

    site changes

--- scripts/app-e5292314.js
+++
@@ -1,3 +0,0 @@
-/******/!function(t){function r(e){if(o[e])return o[e].exports;var n=o[e]={exports:{},id:e,loaded:!1};return t[e].call(n.exports,n,n.exports,r),n.loaded=!0,n.exports}// webpackBootstrap
-/******/
-var o={};return r.m=t,r.c=o,r.p="",r(0)}([function(t,r,o){"use strict";o(1)},function(){"use strict";console.log("x")}]);
